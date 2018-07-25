3cce14b65ce7616dae6689b04731396e30722981
David Darmon <gendarmon@gmail.com>
2018-07-25 16:37:38 -0400

    Updating altboot code.

--- simESN/altboot.py
+++ simESN/altboot.py
@@ -14,7 +14,7 @@ import sidpy
 
 def simulate_ls(N_sim, x, p_opt, k = None):
 	if k is None:
-		k = int(numpy.power(x.shape[0] - p_opt, 4./(4 + 1 + p_opt)))
+		k = int(numpy.power(x.shape[0], 0.5)) # Suggested in Lall-Sharma paper.
 
 		print("Using k = {}.".format(k))
 
@@ -30,19 +30,25 @@ def simulate_ls(N_sim, x, p_opt, k = None):
 
 	nbrs = NearestNeighbors(n_neighbors=k, algorithm='ball_tree').fit(X[:, :-1])
 
+	resampling_weights = 1./numpy.arange(1, k+1)
+	resampling_weights = resampling_weights/numpy.sum(resampling_weights)
+
 	for t in range(N_sim - p_opt):
 		x_cur = x_boot[t:t + p_opt]
 
 		distances, indices = nbrs.kneighbors(x_cur.reshape(1, -1))
 
-		x_boot[t + p_opt] = X[numpy.random.choice(indices[0][1:], size = 1), -1]
+		distances = distances.flatten()
+		indices = indices.flatten()
+
+		x_boot[t + p_opt] = X[numpy.random.choice(indices, size = 1, p = resampling_weights), -1]
 		# x_boot[t + p_opt] = numpy.random.randn(1)*0.025 + X[numpy.random.choice(indices[0][1:], size = 1), -1]
 
 	return x_boot
 
 def simulate_rap(N_sim, x, p_opt, k = None):
 	if k is None:
-		k = int(numpy.power(x.shape[0] - p_opt, 4./(4 + 1 + p_opt)))
+		k = int(numpy.power(x.shape[0] - p_opt, 4./(4 + 1 + p_opt)))+1
 
 		print("Using k = {}.".format(k))
 
@@ -65,6 +71,14 @@ def simulate_rap(N_sim, x, p_opt, k = None):
 
 		distances = distances.flatten()
 		indices = indices.flatten()
+
+		if distances[0] == 0:
+			distances = distances[1:]
+			indices = indices[1:]
+
+		else:
+			distances = distances[:-1]
+			indices = indices[:-1]
 
 		sample_prob = 1/distances # Sample weighted by the reciprocal of the distance to the target point.
 
